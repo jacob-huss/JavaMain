ab8d83519fbd09bd4594389d7bec4cc6c7c193da
jacob-huss <72151699+jacob-huss@users.noreply.github.com>
2020-10-10 10:11:23 -0500

    Lab1

---
+++ src/MainR.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+/**
+ * MainR
+ */
+public class MainR {
+    public static void main(String[] Args) {
+
+
+        Math.random();
+
+        int randOne = (int)(Math.random() * 100 );
+
+        if (randOne > 70) {
+            System.out.println("Big Number");
+            if (randOne < 40) {
+                System.out.println("Small Number");
+                if (randOne >= 40); {
+                    if (randOne <= 70) {
+                    
+                        System.out.println("Medium Number");
+    
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+
+    
+}
